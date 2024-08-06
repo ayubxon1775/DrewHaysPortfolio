920a159716b3d2500c7b7efbb01362c5acdeb33e
Ayubxon <ayubxonaxmatxonov@gmail.com>
2024-08-06 23:45:06 +0500

    added preloaderr

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+"use strict"
+
+// Preloader
+
+const preloader = document.querySelector ("[data-preloader]");
+
+window.addEventListener('DOMContentLoaded', function (){
+  preloader.classList.add("loaded");
+  document.body.classList.add ("loaded")
+})
